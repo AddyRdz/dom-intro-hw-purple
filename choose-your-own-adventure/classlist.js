11d0e89806f4583f1192ab27d4dd3c5239759ea8
Addy Rodriguez <rdz.33.a@gmail.com>
2021-12-22 00:12:28 -0600

    dom-content

--- choose-your-own-adventure/classlist.js
+++ choose-your-own-adventure/classlist.js
@@ -5,16 +5,16 @@
 
 // 1. Get the element with the `elements` class from the DOM and save it to
 //    a variable.
-const element = document.getElementsByClassName('elements')
-console.log(element)
+// const element = document.getElementsByClassName('elements')
+// console.log(element)
 
 // 2. Add the class `this-is-fun` to the .elements node
-newItem.element = "this-is-fun"
-console.log(newItem)
+// newItem.element = "this-is-fun"
+// console.log(newItem)
 
 // 3. Add the class `disco-party` to the .elements node
-newItem.element = 'disco-party'
-console.log(newItem)
+// newItem.element = 'disco-party'
+// console.log(newItem)
 
 // 4. Remove the class `elements` from the .elements node
-remove('elements')
+// remove('elements')
--- dom_content.js
+++ dom_content.js
@@ -6,11 +6,15 @@
 // 1. Get the element with an ID of "first" and save it to a variable called
 //    first (use any method you'd like!).
 const element =document.getElementById('first');
-newId.getAttribute('first',first);
-console.log(element.getAttribute(first))
+newElement.setAttribute("first")
 
 // 2. Print just the text of `first`.
 // 3. Print the HTML of `first`.
 // 4. Get the section tag from the page and save it to a variable.
 // 5. Print the text content of the section tag
+function printItems(sectionTag,content){
+    const target = document.querySelector(sectionTag)
+}
 // 6. Print the HTML content of the section tag
+function printItems(sectionStrng,content){
+    const target = document.querySelector(sectionStrng)
