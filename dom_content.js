01493b582dff55e27f754821e6277c912e06cb92
Addy Rodriguez <rdz.33.a@gmail.com>
2021-12-21 23:53:23 -0600

    classlist

--- dom_content.js
+++ dom_content.js
@@ -5,6 +5,10 @@
 
 // 1. Get the element with an ID of "first" and save it to a variable called
 //    first (use any method you'd like!).
+const element =document.getElementById('first');
+newId.getAttribute('first',first);
+console.log(element.getAttribute(first))
+
 // 2. Print just the text of `first`.
 // 3. Print the HTML of `first`.
 // 4. Get the section tag from the page and save it to a variable.
